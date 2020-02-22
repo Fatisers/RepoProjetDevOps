b5074b0ed2ed47944dce462aefaa039f96ae504d
Sersouri Fatima Zohra <fati.sersouri@gmail.com>
2020-02-22 20:27:46 +0100

    Add composant ajouter bassin

---
+++ src/main/angular/src/app/secteur/add-secteur/add-secteur.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddSecteurComponent } from './add-secteur.component';
+
+describe('AddSecteurComponent', () => {
+  let component: AddSecteurComponent;
+  let fixture: ComponentFixture<AddSecteurComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddSecteurComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddSecteurComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
