a8b1624f13531e6183b4dd0663b555eb6a0282cc
Sersouri Fatima Zohra <fati.sersouri@gmail.com>
2020-02-25 22:32:28 +0100

    Update du menu

--- src/main/angular/src/app/app-routing.module.ts
+++ src/main/angular/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddActiviteComponent } from './activite/add-activite/add-activite.component';
+import { ShowActiviteComponent } from './activite/show-activite/show-activite.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'addActivite', component: AddActiviteComponent},
+  { path: 'showActivite',        component: ShowActiviteComponent },
+  { path: '',   redirectTo: '/addActivite', pathMatch: 'full' },
+  { path: '**', component: AddActiviteComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+ 
+//  { path: '**', component: PageNotFoundComponent}
