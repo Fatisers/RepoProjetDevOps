780b755cc710e553f8d51ca02ebe7c59696c4857
Sersouri Fatima Zohra <fati.sersouri@gmail.com>
2020-03-02 00:19:31 +0100

    Modification du menu selon les roles / login est integre dans menu

--- src/main/angular/src/app/menu/menu/menu.component.ts
+++ src/main/angular/src/app/menu/menu/menu.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ServiceEmployeService } from 'src/app/employe/service-employe.service';
+import { Employe } from 'src/app/interfaces/Employe';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  employe : Employe = {"idEmploye" : 0,"nom": "", "identifiant": "", "motdepasse":"", "prenom": "","adresse":"",
+  "dateDeNaissance": "",
+  "numSecuSociale": "",
+  "role": "Visiteur","listebassins": []};
 
-  constructor() { }
+  showForm : boolean=true
+  loginbool : boolean=true
+  
+  constructor(private ngZone: NgZone,
+    private formBuilder: FormBuilder,
+    private service: ServiceEmployeService) { }
 
   ngOnInit() {
+  }
+
+  login(){
+    console.log("fffffffffff")
+    this.showForm=false
+
+  }
+
+
+  loginForm = this.formBuilder.group({
+    nom: '',
+    identifiant: '',
+    motdepasse:'',
+    prenom: '',
+    adresse:'',
+    dateDeNaissance: '',
+    numSecuSociale: '',
+    role: ''
+  });
+
+  onFormSubmit() {
+    this.showForm=true
+    this.service.login(this.loginForm.value).subscribe(
+      data => this.employe = data,
+      error => console.log("Erreur 3"+error) 
+      )
+      console.log(this.employe)
+      this.loginbool = false
   }
 
 }
